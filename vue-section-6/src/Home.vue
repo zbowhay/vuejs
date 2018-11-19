409bb71f8ac2843e35d36c66f9ecda82892a8f83
Zachary Bowhay <zbowhay@gmail.com>
2018-11-19 15:40:44 -0600

    starting components and assignment 6

---
+++ vue-section-6/src/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app-servers">
+    <app-server-status v-for="server in 5" :key="server"></app-server-status>
+  </div>
+</template>
+
+<script>
+  import ServerStatus from './ServerStatus.vue';
+  export default {
+    components: {
+      'app-server-status': ServerStatus
+    }
+  }
+</script>
---
+++ vue-section-6/src/ServerStatus.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="ServerStatus">
+    <h1>Server Status: {{status}}</h1>
+    <hr>
+    <button @click="changeStatus">Change Status</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return { status: 'Critical' };
+  },
+  methods: {
+    changeStatus() {
+      this.status = 'Normal';
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
